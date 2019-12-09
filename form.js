f08e9c529e9fa76757ed0bdb55f29d9e29f03742
Angelia <angelia.tk@gmail.com>
2019-12-09 15:13:50 +0800

    included YouTube source and modified HTML code

--- form.js
+++ form.js
@@ -1,62 +1,32 @@
 $(document).ready(function () {
-    // $("#loading").hide();
     $("#frmSearch").submit(function (e) {
         e.preventDefault();
-        var i = 0;
-        let query = $("#txtQuery").val();
-        findRecipe(query);
-        // var alertMsg = "";
-        // alertMsg += $("#meat").val();
-        // alert(alertMsg); //What is this and how does it work?
-        // console.log(alertMsg)
-        // })
+        let food = $("#txtQuery").val();
+        findRecipe(food);
     });
 
-    $(document).on("click", "a", function () {
-        console.log($(this).data("title"));
+    function findRecipe(food) {
 
-        var title = $(this).data("title");
-
-    });
-
-
-
-    function findRecipe(query) {
-        console.log("finding");
         var settings = {
             "async": true,
             "crossDomain": true,
-            "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/4632/summary" + query,
+            "url": `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/videos/search?query=${food}&minLength=0&maxLength=999&offset=0&number=10`,
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
                 "x-rapidapi-key": "28b07e21ccmsh9cf791b8dac4546p121d9fjsna67b0a0ef7c0",
-            },
-            // "beforeSend": function(xhr) {
-            //     // UI changes before the ajax request query is sent
-            //     $("#loading").show();
-            // 
+            }
         }
+       
 
-        $.ajax()
-            .done(function () {
-                console.log("you");
-                // // reverse back UI changes
-                // $("#loading").hide();
-                // console.log(response)
+        $.ajax(settings).done(function (response) {
+            for (var i = 6; i < response.length; i++);
+            var obj = response[i];
+            // $("#response").html(response[i]);
+            // $("#response").html(response["videos"][0]["title"]);
+            let newResponse = response['videos'][0]["youTubeId"];
+            $("#frame").attr("src","https://www.youtube.com/embed/"+ newResponse);
 
-                let responseSummary = ""; //define the variable
-
-                for (var i = 0; i < response.length; i++) { //loop through the entire function
-                    var obj = response[i];
-
-                    responseSummary += response[i]["title"]; //include keys
-                    //             }
-                    $("#response").html(responseSummary); //results in html
-
-                }
-
-            });
-
+        });
     }
 });
